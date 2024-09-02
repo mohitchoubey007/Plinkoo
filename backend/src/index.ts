7a14a29f619f7295580a5205a7a099ae73cbd399
Mohit Choubey <mohitjdh2021@gmail.com>
2024-09-02 15:34:12 +0530

    changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
-import * as express from "express";
-import * as cors from "cors";
+import express, { Request, Response } from "express";
+import cors from "cors";
 import { outcomes } from "./outcomes";
+
 const app = express();
 app.use(cors());
 
@@ -26,7 +27,7 @@ const MULTIPLIERS: { [key: number]: number } = {
   16: 16,
 };
 
-app.post("/game", (req, res) => {
+app.post("/game", (req: Request, res: Response) => {
   let outcome = 0;
   const pattern = [];
   for (let i = 0; i < TOTAL_DROPS; i++) {
